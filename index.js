e94b34cab6803f1762e8fccbfcdf9e66189cc9a7
tankcccc <540153461@qq.com>
2019-06-18 11:43:20 +0800

    移动端适配

--- index.js
+++ index.js
@@ -37,12 +37,80 @@ autoSetCanvasSize(canvas)
 
 
 
-var using = false
-var lastPoint={x:undefined,y:undefined}
-listenToMouse(canvas)
+if (document.body.ontouchstart !== undefined) {
+    //触屏设备
+    canvas.ontouchstart=function(event){
+        var x = event.touches[0].clientX
+        var y = event.touches[0].clientY
+        // drawCircle(x,y,5)
+        using=true
+        if (eraserEnabled) {
+        
+            context.clearRect(x-5,y-5,10,10)
+        } else {
+            lastPoint={x:x,y:y}
+        }
+    }
+    canvas.ontouchmove=function(event){
+        var x = event.touches[0].clientX
+        var y = event.touches[0].clientY  
+        if (eraserEnabled) {
+            if (using) {
+                context.clearRect(x-5,y-5,10,10)
+            }
+        } 
+        else {
+            if (using) {
+                var newPoint={
+                    x:x,
+                    y:y
+                }
+                drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+                lastPoint=newPoint
+            }
+        }
+    }
+    
+    canvas.ontouchend=function(event){
+        using=false
+    }
 
+} else {
+    //非触屏设备
+    var using = false
+    var lastPoint={x:undefined,y:undefined}
+    listenToMouse(canvas)
+}
 
+function drawCircle(x,y,radius){
+    context.beginPath()
+    context.fillStyle='black'
+    context.arc(x,y,radius,0,Math.PI*2)
+    context.fill()
+}
+function drawLine(x1,y1,x2,y2){
+    context.beginPath()
+    context.strokeStyle='black'
+    context.moveTo(x1,y1)
+    context.lineWidth = 5
+    context.lineTo(x2,y2)
+    context.stroke()
+    context.closePath()
+}
 
+//全屏问题
+function autoSetCanvasSize(canvas){
+    setCanvasSize()
+    window.onresize=function(){
+    setCanvasSize()
+    }
+    function setCanvasSize (event){
+        var pageWidth=document.documentElement.clientWidth
+        var pageHeight=document.documentElement.clientHeight
+        canvas.width=pageWidth
+        canvas.height=pageHeight
+    }
+}
 // 所有实践的封装
 function listenToMouse(canvas){
     canvas.onmousedown=function(event){
@@ -51,7 +119,7 @@ function listenToMouse(canvas){
         // drawCircle(x,y,5)
         using=true
         if (eraserEnabled) {
-           
+        
             context.clearRect(x-5,y-5,10,10)
         } else {
             lastPoint={x:x,y:y}
@@ -81,36 +149,6 @@ function listenToMouse(canvas){
         using=false
     }
     
-}
-
-function drawCircle(x,y,radius){
-    context.beginPath()
-    context.fillStyle='black'
-    context.arc(x,y,radius,0,Math.PI*2)
-    context.fill()
-}
-function drawLine(x1,y1,x2,y2){
-    context.beginPath()
-    context.strokeStyle='black'
-    context.moveTo(x1,y1)
-    context.lineWidth = 5
-    context.lineTo(x2,y2)
-    context.stroke()
-    context.closePath()
-}
-
-//全屏问题
-function autoSetCanvasSize(canvas){
-    setCanvasSize()
-    window.onresize=function(){
-    setCanvasSize()
-    }
-    function setCanvasSize (event){
-        var pageWidth=document.documentElement.clientWidth
-        var pageHeight=document.documentElement.clientHeight
-        canvas.width=pageWidth
-        canvas.height=pageWidth
-    }
 }
 
 
